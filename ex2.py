e1c8bbaf84c609fd644238bfda1321e6308d1586
Botalov Serg <89090198690@mail.ru>
2023-03-27 21:19:53 +0500

    add new file

---
+++ ex2.py
@@ -0,0 +1,11 @@
+import smtplib as smpt
+from getpass import getpass
+from pass_email import pass_email
+
+ 
+email = 'promconsulting96@mail.ru'
+password = pass_email
+
+dest_email = "89090168690@mail.ru"
+
+subject = 'IP'
