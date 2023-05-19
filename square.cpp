753892495615f3c475fc5ad3061dd1a03dcf9d72
Botalov Serg <89090168690@mail.ru>
2023-05-19 12:09:10 +0500

    fix

--- square.cpp
+++ square.cpp
@@ -1,4 +1,30 @@
-int square(int x)
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main()
 {
-    return x * x;
+    int a,b,c,d;
+    
+
+    cin >> a >> b >> c;
+
+    d = (b * b) - (4*a*c);
+
+    if (d < 0) {
+        cout << "No real roots" << endl;
+    } else if (d == 0) {
+        double x_1;
+        x_1 = (double)(-b) / (2 * a);
+        cout <<  x_1 << " " << x_1 << endl;
+    } else {
+        double x_1, x_2;
+        x_1 = (double)(-b - sqrt(d)) / (2 * a);
+        x_2 = (double)(-b + sqrt(d)) / (2 * a);
+        cout << x_1 << " " << x_2 << endl;
+    }
+
+    return 0;
 }
--- square.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef SQUARE_HPP
-#define SQUARE_HPP
-
-int square(int x);
-
-#endif
